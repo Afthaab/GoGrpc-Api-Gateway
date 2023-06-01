52ceef7c96aff5ea88d92b3b795a2de1ecbd76a9
Afthab <mohammedafthab15@gmail.com>
2023-06-01 19:56:30 +0530

    jwt validation has been completed

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/apiGateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthMiddlewareConfig struct {
+	svc *ServiceAuth
+}
+
+func InitAuthMiddleware(Svc *ServiceAuth) AuthMiddlewareConfig {
+	return AuthMiddlewareConfig{
+		svc: Svc,
+	}
+}
+
+func (c *AuthMiddlewareConfig) AuthRequired(ctx *gin.Context) {
+	authorization := ctx.Request.Header.Get("authorization")
+
+	if authorization == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	token := strings.Split(authorization, "Bearer ")
+	if len(token) < 2 {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	res, err := c.svc.client.Validate(context.Background(), &pb.ValidateRequest{
+		Accesstoken: token[1],
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	}
+
+	ctx.Writer.Header().Set("userId", fmt.Sprint(res.Userid))
+	ctx.Next()
+}
